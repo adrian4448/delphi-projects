87960c7267d0932b68bd739a386eb8296c5925b4
adrian4448 <adrian.morais4448@gmail.com>
2021-03-15 22:31:28 -0300

    Aula 03 - Iniciando exercicio 03

--- Exercicio 03/PR_cidade.dpr
+++ Exercicio 03/PR_cidade.dpr
@@ -6,7 +6,8 @@ uses
   unit_data in 'unit_data.pas' {data_module: TDataModule},
   objeto_habitante in 'objeto_habitante.pas',
   habitante_service in 'habitante_service.pas',
-  unit_cadastro in 'unit_cadastro.pas' {form_cadastro};
+  unit_cadastro in 'unit_cadastro.pas' {form_cadastro},
+  unit_resultado in 'unit_resultado.pas' {form_results};
 
 {$R *.res}
 
